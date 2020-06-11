05133c5dd5e14bcd263245d0f1638aef5bd91495
jfabiant <john.timoteo@tecsup.edu.pe>
2020-06-10 21:24:57 -0500

    fix Mapa.xaml

---
+++ RurayFoodApp.Android/Renderer/CustomEditorRenderer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using RurayFoodApp.CustomRenderer;
+using RurayFoodApp.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomEditor), typeof(CustomEditorRenderer))]
+namespace RurayFoodApp.Droid.Renderer
+{
+    public class CustomEditorRenderer : EditorRenderer
+    {
+        public CustomEditorRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
---
+++ RurayFoodApp/CustomRenderer/CustomEditor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RurayFoodApp.CustomRenderer
+{
+    public class CustomEditor:Editor
+    {
+    }
+}
